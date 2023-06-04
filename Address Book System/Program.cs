1232efd9fcd46ffc598c479892d75f3ab729d5f8
Rohan <rohandute1@gmail.com>
2023-06-04 10:10:17 +0530

    [Rohan] Tab for contact editing created successsfully

--- Address Book System/Program.cs
+++ Address Book System/Program.cs
@@ -18,7 +18,7 @@ namespace Address_Book_System
             {
                 Console.WriteLine("Address Book Menu");
                 Console.WriteLine("--------------------");
-                Console.WriteLine(" 1. Add Contact\n 2. Display All Contacts\n 3. Exit");
+                Console.WriteLine(" 1. Add Contact\n 2. Display All Contacts\n 3. Edit Contact\n 4. Exit");
 
                 Console.WriteLine("Enter your choice: ");
                 int choice = Convert.ToInt32(Console.ReadLine());
@@ -32,6 +32,9 @@ namespace Address_Book_System
                         addressBook.DisplayContacts();
                         break;
                     case 3:
+                        addressBook.EditContact();
+                        break;
+                    case 4:
                         exit = true;
                         break;
                     default:
@@ -55,7 +58,6 @@ namespace Address_Book_System
         public void AddContact()
         {
             Console.WriteLine("Enter Contact Details");
-
             Console.WriteLine("First Name: ");
             string firstName = Console.ReadLine();
 
@@ -103,18 +105,58 @@ namespace Address_Book_System
                 Console.WriteLine();
             }
         }
+
+        public void EditContact()
+        {
+            Console.WriteLine("Enter the name of the contact you want to edit");
+            Console.WriteLine("First Name: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Last Name: ");
+            string lastName = Console.ReadLine();
+
+            Contact contactToEdit = Contacts.Find(c => c.FirstName == firstName && c.LastName == lastName);
+
+            if (contactToEdit != null)
+            {
+                Console.WriteLine("Enter new contact details");
+                Console.WriteLine("Address: ");
+                contactToEdit.Address = Console.ReadLine();
+
+                Console.WriteLine("City: ");
+                contactToEdit.City = Console.ReadLine();
+
+                Console.WriteLine("State: ");
+                contactToEdit.State = Console.ReadLine();
+
+                Console.WriteLine("ZIP: ");
+                contactToEdit.Zip = Console.ReadLine();
+
+                Console.WriteLine("Phone Number: ");
+                contactToEdit.PhoneNumber = Console.ReadLine();
+
+                Console.WriteLine("Email: ");
+                contactToEdit.Email = Console.ReadLine();
+
+                Console.WriteLine("Contact updated successfully");
+            }
+            else
+            {
+                Console.WriteLine("Contact not found");
+            }
+        }
     }
 
     class Contact
     {
         public string FirstName { get; }
         public string LastName { get; }
-        public string Address { get; }
-        public string City { get; }
-        public string State { get; }
-        public string Zip { get; }
-        public string PhoneNumber { get; }
-        public string Email { get; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
 
         public Contact(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
         {
